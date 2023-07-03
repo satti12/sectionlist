898704259edc60c5cdcf400b1f556ddbc2ed5051
satti12 <muqadsasatti81@gmail.com>
2023-07-02 22:27:51 -0700

    Sectin list code

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, SectionList, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const data = [
+    {
+      title: 'Fruits',
+      data: ['Apple', 'Mango', 'Banana'],
+    },
+    {
+      title: 'Vegetables',
+      data: ['Carrot', 'Tomato', 'Broccoli'],
+    },
+    {
+      title: 'Colors',
+      data: ['Red', 'Green', 'Blue'],
+    },
+  ];
+
+  const renderSectionHeader = ({ section }) => {
+    return (
+      <View style={styles.sectionHeader}>
+        <Text style={styles.sectionHeaderText}>{section.title}</Text>
+      </View>
+    );
+  };
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.itemText}>{item}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SectionList
+        sections={data}
+        renderSectionHeader={renderSectionHeader}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
+    paddingTop: 24,
+  },
+  sectionHeader: {
+    backgroundColor: 'blue',
+    padding: 16,
+  },
+  sectionHeaderText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  item: {
+    padding: 16,
+    paddingLeft: 30, // Add left padding of 20 pixels
+  },
+  itemText: {
+    fontSize: 16,
   },
 });
+
+export default App;
